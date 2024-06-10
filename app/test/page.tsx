93dbfbd9a9fcd4bd211098100847fbaf8335ae73
elsayed-hussein <elsayed.hussen@gmail.com>
2024-06-11 00:28:29 +0300

    add todo.md

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -3,7 +3,13 @@ import React from "react";
 type Props = {};
 
 const page = (props: Props) => {
-  return <div className="m-a">page</div>;
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
+        <a href="http://localhost:3000/">localhost-Home</a>
+      </div>
+    </main>
+  );
 };
 
 export default page;
